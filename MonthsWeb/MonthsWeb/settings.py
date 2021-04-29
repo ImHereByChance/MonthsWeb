91792a1745dfc3e858af63d9734e3e2733925d76
ImHereByChance <kadermetov@gmail..com>
2021-04-29 21:10:13 +0300

    template issue resolved

--- MonthsWeb/MonthsWeb/settings.py
+++ MonthsWeb/MonthsWeb/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'MonthsWeb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'taskmanager/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
