a576ec98d1bc0e21669ac5eac6b7beae6bd75878
ImHereByChance <kadermetov@gmail..com>
2021-05-09 04:35:09 +0300

    add checking the uniqueness of the email field in the user detail changing form.

--- MonthsWeb/users/forms.py
+++ MonthsWeb/users/forms.py
@@ -7,11 +7,30 @@ from django.utils.translation import gettext as _
 
 class UserDetailsChangingForm(forms.ModelForm):
     """Change username, useremail, etc. (except the password)."""
-    
+
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name',)
 
+    def clean_email(self):
+        new_email = self.cleaned_data['email']
+        if User.objects\
+            .filter(email=new_email)\
+            .exclude(id=self.instance.id)\
+            .exists():
+            raise ValidationError(
+                _('A user with that email already exists.'),
+                code='not_unique_email'
+            )
+        return new_email
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+    
 
 class RegisterForm(UserCreationForm):
     """ A form to create new Users.
