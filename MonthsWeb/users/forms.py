5e00398e69c63110b8de598ba752122d4582bbd6
ImHereByChance <kadermetov@gmail..com>
2021-05-09 03:08:10 +0300

    add checking the uniqueness of the email field in the user registration form.

--- MonthsWeb/users/forms.py
+++ MonthsWeb/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UsernameField
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.utils.translation import gettext as _
 
 
@@ -12,6 +13,25 @@ class UserDetailsChangingForm(forms.ModelForm):
         fields = ('username', 'email', 'first_name', 'last_name',)
 
 
-
 class RegisterForm(UserCreationForm):
+    """ A form to create new Users.
+    Extended from django.contrib.auth.forms.UserCreationForm
+    """
+    
     email = forms.EmailField()
+
+    def clean_email(self):
+        new_email = self.cleaned_data['email']
+        if User.objects.filter(email=new_email).exists():
+            raise ValidationError(
+                _('A user with that email already exists.'),
+                code='not_unique_email'
+            )
+        return new_email
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
