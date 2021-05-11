52d72734b0ef196a14ba60627e0978db96cc0041
ImHereByChance <kadermetov@gmail..com>
2021-05-11 06:18:58 +0300

    the docstring was corrected

--- MonthsWeb/users/services/userprofileservice.py
+++ MonthsWeb/users/services/userprofileservice.py
@@ -4,7 +4,7 @@ from ..models import UserProfile
 
 def set_user_profile(user: User, **kwargs: dict) -> None:
     """Set the values of the UserProfile fields (the model related to
-    the User and which stores personal settings). The first positional
+    the User which stores personal settings). The first positional
     argument must be a User object, the rest must be keyword arguments,
     which are `models.UserProfile` fields. 
     """
