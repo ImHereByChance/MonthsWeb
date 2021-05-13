13ad13e2a16e3982ecb566c0f9367c082564a836
ImHereByChance <kadermetov@gmail..com>
2021-05-13 06:04:09 +0300

    redirects on the user settings page now work as they should to work

--- MonthsWeb/users/urls.py
+++ MonthsWeb/users/urls.py
@@ -5,7 +5,4 @@ from . import views
 urlpatterns = [
     path('register', views.register, name='register'),
     path('user_settings/', views.user_settings, name='user_settings'),
-    path('user_details_change/', views.change_user_details, name='user_details_change'),
-    path('password_change/', views.change_password, name='password_change'),
-    
 ]
--- MonthsWeb/users/views.py
+++ MonthsWeb/users/views.py
@@ -1,11 +1,12 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordChangeForm
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from django.urls import reverse
 
 from .forms import RegisterForm, UserDetailsChangingForm
+from .services.misc import dispatch_messages
 
 
 def register(request):
@@ -27,77 +28,79 @@ def register(request):
 
 
 @login_required
-def change_user_details(request):
-    """ A view to handle the form that in charge of changing the
-    fields of the User model (excluding password).
-    """
-    if request.method == 'POST':
-        form = UserDetailsChangingForm(request.POST, instance=request.user)
-        
-        context = {
-            'user': request.user,
-            'user_details_form': form,
-            'password_change_form': PasswordChangeForm(request.user),
-        }
-
-        if form.is_valid():
-            form.save()
-            context['user_detail_canged_msg'] = 'changes saved'
-        
-        return render(request, 'registration/user_settings.html', context)
-            
-    else:
-        return HttpResponse(status=405)
-    
-
-@login_required
-def change_password(request):
-    """ A view to handle the form for userpasswords changing. """
-
-    if request.method == 'POST':
-        password_canged_form = PasswordChangeForm(request.user,
-                                                  request.POST)
-        context = {
-            'user': request.user,
-            'user_details_form': UserDetailsChangingForm(instance=request.user),
-            'password_change_form': password_canged_form,
-        }
-
-        if password_canged_form.is_valid():
-            password_canged_form.save()
-            username = request.user.username
-            password = password_canged_form.cleaned_data['new_password1']
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            context['password_changed_msg'] = 'password changed'
-        
-        return render(request, 'registration/user_settings.html', context)
-
-    else:
-        return HttpResponse(status=405)
-    
-
-@login_required
 def user_settings(request):
-    """ The view that returns the page with usersettings. Settings are
-    made up of individual forms that are handled by their own views
-    (current view only renders the page with thouse forms - not handles
-    them, hence the only allowed method is get).
+    """View to handle the page where users can change their preferences
+    and details. The page contains multiple forms which have their own 
+    submit-inputs (all of them are being handled here).
     """
-    
-    if not request.method == 'GET':
+
+    if request.method == 'GET':
+        user_details_form = UserDetailsChangingForm(instance=request.user)
+        password_change_form = PasswordChangeForm(user=request.user)
+
+        context = {
+            'user': request.user,
+            'user_details_form': user_details_form,
+            'password_change_form': password_change_form
+        }
+
+        message_storage = messages.api.get_messages(request)
+        if message_storage:
+            messages_dict = dispatch_messages(message_storage)
+            context.update(messages_dict)
+
+        return render(request, 'registration/user_settings.html', context)
+
+    elif request.method == 'POST':
+        
+        form_name = request.POST.get('form_name')  # <input type="hidden" ...
+
+        if form_name == 'user_details_form':
+            user_details_form = UserDetailsChangingForm(data=request.POST,
+                                                        instance=request.user)
+            password_change_form = PasswordChangeForm(user=request.user)
+        elif form_name == 'password_change_form':
+            password_change_form = PasswordChangeForm(data=request.POST,
+                                                      user=request.user)
+            user_details_form = UserDetailsChangingForm(instance=request.user)
+        else:
+            raise Exception('Cannot define which form was submitted')
+
+        context = {
+            'user': request.user,
+            'user_details_form': user_details_form,
+            'password_change_form': password_change_form,
+            'submitted_form': form_name
+        }    
+
+        for form in (user_details_form, password_change_form):
+            if form.is_bound:
+                if not form.is_valid():
+                    for field_name, error_list in form.errors.items():
+                        for err_msg in error_list:
+                            messages.error(request=request,
+                                           message=err_msg,
+                                           extra_tags=field_name)
+                    return redirect('user_settings')
+
+                elif form_name == 'user_details_form':
+                    messages.success(request=request,
+                                     message='Successfully Changed',
+                                     extra_tags='user_details_form')
+                    form.save()
+
+                elif form_name == 'password_change_form':
+                    messages.success(request=request,
+                                     message='Successfully Changed',
+                                     extra_tags='password_change_form')
+                    form.save()
+
+                    username = request.user.username
+                    password = form.cleaned_data['new_password1']
+                    user = authenticate(username=username, password=password)
+                    login(request, user)
+
+        return redirect('user_settings')
+
+    else:
         return HttpResponse(status=405)
-
-    user_details_form = UserDetailsChangingForm(instance=request.user)
-    password_change_form = PasswordChangeForm(request.user)
-    
-    context = {
-        'user': request.user,
-        'password_change_form': password_change_form,
-        'user_details_form': user_details_form,
-    }
-
-    return render(request, 'registration/user_settings.html', context)
-
-
-
