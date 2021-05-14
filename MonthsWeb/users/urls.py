238a4e692d7c5fcf23ae6e19cb75a47d8892c740
ImHereByChance <kadermetov@gmail..com>
2021-05-14 20:54:53 +0300

    users.views.user_settings refactored multiple function based views

--- MonthsWeb/users/urls.py
+++ MonthsWeb/users/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('register', views.register, name='register'),
     path('user_settings/', views.user_settings, name='user_settings'),
+    path('change_user_password', views.change_user_password, name='change_user_password'),
+    path('change_user_details', views.change_user_details, name='change_user_details')
 ]
